b5597e52346ce82c5b952dbcfa3aa111a049f9a7
rainbow954 <rainbow954@163.com>
2018-03-27 00:30:03 +0800

    update

---
+++ mesos-elastic-job/mesos-elasticjob-latest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export HOST_IP=172.19.0.37
+export ZK_IP=172.19.0.21:2181,172.19.0.22:2181,172.19.0.23:2181
+export event_trace_rdb_driver=com.mysql.jdbc.Driver
+export event_trace_rdb_url=jdbc:mysql://172.19.0.10:3306/elastic_job_cloud_log
+export mesos_url=zk://172.19.0.21:2181/mesos
+sudo docker run -d \
+-e hostname=${HOST_IP} \
+-e mesos_url=${mesos_url} \
+-e zk_servers=${ZK_IP} \
+-e event_trace_rdb_driver=${event_trace_rdb_driver} \
+-e event_trace_rdb_url=${event_trace_rdb_url} \
+-e event_trace_rdb_username=root \
+-e event_trace_rdb_password=root \
+--name mesos-elsticjob --net shadownet --ip ${HOST_IP}  rainbow954/mesos-elasticjob:latest
+
+
+
+
---
+++ mesos-elastic-job/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+
+IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
+cat /elastic-job/elastic-job-cloud-scheduler.properties.template | sed \
+  -e "s|{{hostname}}|${hostname:-$IP}|g" \
+  -e "s|{{mesos_url}}|${mesos_url}|g" \
+  -e "s|{{zk_servers}}|${zk_servers}|g" \
+  -e "s|{{event_trace_rdb_driver}}|${event_trace_rdb_driver}|g" \
+  -e "s|{{event_trace_rdb_url}}|${event_trace_rdb_url}|g" \
+  -e "s|{{event_trace_rdb_username}}|${event_trace_rdb_username:root}|g" \
+  -e "s|{{event_trace_rdb_password}}|${event_trace_rdb_password:root}|g" \
+   > /elastic-job/elastic-job-cloud-scheduler/conf/elastic-job-cloud-scheduler.properties
+
+
+
+echo "Starting elastic-job-cloud"
+service ssh start
+exec //elastic-job/elastic-job-cloud-scheduler/bin/start.sh
