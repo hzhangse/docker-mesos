cc9d059c199fb52c5927a1d4f0d0719c24fb2e84
rainbow954 <rainbow954@163.com>
2018-03-27 13:46:35 +0800

    sdfa

--- mesos-elastic-job/start.sh
+++ mesos-elastic-job/start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-
-IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
-
-cat /elastic-job/elastic-job-cloud-scheduler.properties.template | sed \
-  -e "s|{{hostname}}|${hostname:-$IP}|g" \
-  -e "s|{{mesos_url}}|${mesos_url}|g" \
-  -e "s|{{zk_servers}}|${zk_servers}|g" \
-  -e "s|{{event_trace_rdb_driver}}|${event_trace_rdb_driver}|g" \
-  -e "s|{{event_trace_rdb_url}}|${event_trace_rdb_url}|g" \
-  -e "s|{{event_trace_rdb_username}}|${event_trace_rdb_username:root}|g" \
-  -e "s|{{event_trace_rdb_password}}|${event_trace_rdb_password:root}|g" \
-   > /opt/elastic-job-cloud-scheduler/conf/elastic-job-cloud-scheduler.properties
-
-
-
-echo "Starting elastic-job-cloud"
-service ssh start
-#exec /opt/elastic-job-cloud-scheduler/bin/start.sh
-
 cd `dirname $0`
 cd ..
 DEPLOY_DIR=`pwd`
