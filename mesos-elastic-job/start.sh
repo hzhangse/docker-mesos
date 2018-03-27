2e2fadede42d8cc73e9ed03915c80920c530fc92
rainbow954 <rainbow954@163.com>
2018-03-27 13:32:46 +0800

    aaa

--- mesos-elastic-job/start.sh
+++ mesos-elastic-job/start.sh
@@ -17,20 +17,3 @@ cat /elastic-job/elastic-job-cloud-scheduler.properties.template | sed \
 
 echo "Starting elastic-job-cloud"
 service ssh start
-#exec /opt/elastic-job-cloud-scheduler/bin/start.sh
-
-cd `dirname $0`
-cd ..
-DEPLOY_DIR=`pwd`
-CONF_DIR=${DEPLOY_DIR}/conf
-LIB_DIR=${DEPLOY_DIR}/lib/*
-CONTAINER_MAIN=io.elasticjob.cloud.scheduler.Bootstrap
-JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=6666,server=y,suspend=n -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.library.path=/usr/local/lib:/usr/lib:/usr/lib64"
-source ${CONF_DIR}/elastic-job-cloud-scheduler.properties
-if [ ${hostname} = "" ] || [ ${hostname} = "127.0.0.1" ] || [ ${hostname} = "localhost" ]; then
-  echo "Please config hostname in conf/elastic-job-cloud-scheduler.properties with a routable IP address."
-  exit;
-fi
-export LIBPROCESS_IP=${hostname}
-
-#java ${JAVA_OPTS} -classpath ${CONF_DIR}/*:${LIB_DIR}:. ${CONTAINER_MAIN}
